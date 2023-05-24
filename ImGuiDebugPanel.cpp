b13e080c64bd80b4a42c888782c2c9fefaa06a0e
Pascal Thomet <pthomet@gmail.com>
2023-05-24 19:22:25 +0200

    Fix clang warnings with ImGui::Text
    (since ImGui::Text uses va_args, clang warns when a format string is not provided)

--- ImGuiDebugPanel.cpp
+++ ImGuiDebugPanel.cpp
@@ -20,7 +20,7 @@ void TextEditor::ImGuiDebugPanel(const std::string& panelName)
 	{
 		static std::string numberOfRecordsText;
 		numberOfRecordsText = "Number of records: " + std::to_string(mUndoBuffer.size());
-		ImGui::Text(numberOfRecordsText.c_str());
+		ImGui::Text("%s", numberOfRecordsText.c_str());
 		ImGui::DragInt("Undo index", &mState.mCurrentCursor);
 		for (int i = 0; i < mUndoBuffer.size(); i++)
 		{
@@ -30,7 +30,7 @@ void TextEditor::ImGuiDebugPanel(const std::string& panelName)
 				ImGui::Text("Operations");
 				for (int j = 0; j < mUndoBuffer[i].mOperations.size(); j++)
 				{
-					ImGui::Text(mUndoBuffer[i].mOperations[j].mText.c_str());
+					ImGui::Text("%s", mUndoBuffer[i].mOperations[j].mText.c_str());
 					ImGui::Text(mUndoBuffer[i].mOperations[j].mType == UndoOperationType::Add ? "Add" : "Delete");
 					ImGui::DragInt2("Start", &mUndoBuffer[i].mOperations[j].mStart.mLine);
 					ImGui::DragInt2("End", &mUndoBuffer[i].mOperations[j].mEnd.mLine);
